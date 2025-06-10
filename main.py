4ce9865f46019beb27f7e8164140989050346b76
bfit3520 <aa5818023@gmail.com>
2025-06-10 23:28:11 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+
+def read_ps():
+    with open("ps.txt", "r", encoding = "utf-8") as f:
+        a = f.readline().replace("\n", "")
+    return a
+
+def read_st():
+    with open("st.txt", "r", encoding = "utf-8") as f:
+        a = f.readline().replace("\n", "")
+    return a
+
+def change(inp):
+    with open("st.txt", "w", encoding = "utf-8") as f:
+        f.write(inp)
+
+
+
+@app.route("/")
+def home():
+    if read_st == '1':
+        return render_template('index.html', status = "선생님께서 계십니다.")
+    else:
+        return render_template('index.html', status = "선생님께서 안 계십니다.")
+
+@app.route("/submit", method = ["POST"])
+def submit():
+    password = request.form['p']
+    if password == read_ps():
+        if read_st() == '1':
+            change('0')
+        else:
+            change('1')
+    
+    if read_st == '1':
+        return render_template('index.html', status = "선생님께서 계십니다.")
+    else:
+        return render_template('index.html', status = "선생님께서 안 계십니다.")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
