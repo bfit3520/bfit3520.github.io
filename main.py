e2e8fc53e82f6b22d4891fadb049788177cdaf34
bfit3520 <aa5818023@gmail.com>
2025-06-11 03:22:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import os
 
 app = Flask(__name__)
 
@@ -36,4 +37,5 @@ def submit():
         return render_template('index.html', status="선생님께서 안 계십니다.")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
